4336d3a710737b259b7a5bbdd4d26a7f7d18d08f
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-09-14 21:11:31 +0545

    added rate limiting

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,15 @@
+# API for fetching quotes!
+# Let's implement kinda rate limiting 
+
 import random
 import json
 from pathlib import Path
 from contextlib import asynccontextmanager
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from fastapi.middleware.cors import CORSMiddleware
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
 
 QUOTE_DATA = []
 
@@ -22,9 +28,15 @@ async def lifespan(app: FastAPI):
         QUOTE_DATA = load_quotes()
     except Exception as e:
         print(f"Error loading quote: {e}")
-        yield
 
-app = FastAPI()
+    yield
+
+limiter = Limiter(key_func=get_remote_address)
+
+app = FastAPI(lifespan=lifespan)
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
@@ -40,7 +52,8 @@ def root():
 
 
 @app.get("/api/quote")
-def harvest():
+@limiter.limit("10/minute")
+def harvest(request: Request):
     if not QUOTE_DATA:
         raise HTTPException(status_code=500, detail="No quote available in database.")
     return random.choice(QUOTE_DATA)
