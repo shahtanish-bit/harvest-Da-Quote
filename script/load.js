eb0f9212da3e3f26e3a49a92abc329cdbe0d7479
shahtanish-bit <shahtanish0165@gmail.com>
2026-09-19 16:34:38 +0545

    made and added a sickel as cursor

--- script/load.js
+++ script/load.js
@@ -1,13 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const testDiv = document.getElementById('wlc-div');
     const btn = document.getElementById('wlc-btn');
-    
-    const hasVisited = sessionStorage.getItem('hasVisitedBefore');
 
-    if (!hasVisited) {
-        testDiv.classList.remove('wlc-hidden');
-        sessionStorage.setItem('hasVisitedBefore', 'true');
-    }
+    testDiv.classList.remove('wlc-hidden');
 
     btn.addEventListener('click', () => {
         testDiv.classList.add('wlc-hidden');
