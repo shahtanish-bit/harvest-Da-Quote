b04d46f3f159165292723d998d3787c90b8798f7
HeartlyTheCoderr <proheartlyplayz@gmail.com>
2026-09-19 19:25:49 +0545

    ayeee

--- script/app.js
+++ script/app.js
@@ -1,19 +1,3 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const testDiv = document.getElementById('wlc-div');
-    const btn = document.getElementById('wlc-btn');
-
-    testDiv.classList.remove('wlc-hidden');
-
-    btn.addEventListener('click', () => {
-        testDiv.classList.add('wlc-hidden');
-    });
-});
-
-
-
-
-
-
 const sickle = document.getElementById('sickle');
 let x = 0, y = 0, ticking = false;
 
